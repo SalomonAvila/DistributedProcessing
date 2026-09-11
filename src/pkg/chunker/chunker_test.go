e07ec3531c45c54c236dbd2dbd214176fbca0424
SalomonAvila <salomon.avila2506@gmail.com>
2026-09-11 08:50:00 -0500

    Fix de columnas de datos y tipos

--- src/pkg/chunker/chunker_test.go
+++ src/pkg/chunker/chunker_test.go
@@ -112,10 +112,13 @@ func TestChunkContractRecords(t *testing.T) {
 }
 
 func TestProcessCSVReader(t *testing.T) {
-	csvData := `id_del_proceso,nit_entidad,proveedores_invitados,proveedores_unicos_con_respuestas,modalidad_de_contratacion,estado_del_procedimiento
-CO1.P1,900123456,10,3,Licitacion Publica,Adjudicado
-CO1.P2,900654321,5,1,Seleccion Abreviada,Adjudicado
-CO1.P3,900111222,8,0,Minima Cuantia,Desierto
+	// Nombres de columna tal como vienen en el CSV real de datos.gov.co
+	// (dataset p6dx-8zbt): "entidad" en vez de "nombre_entidad", y
+	// "proveedores_unicos_con" truncado por Socrata.
+	csvData := `entidad,id_del_proceso,nit_entidad,proveedores_invitados,proveedores_unicos_con,modalidad_de_contratacion,estado_del_procedimiento
+ENTIDAD UNO,CO1.P1,900123456,10,3,Licitacion Publica,Adjudicado
+ENTIDAD DOS,CO1.P2,900654321,5,1,Seleccion Abreviada,Adjudicado
+ENTIDAD TRES,CO1.P3,900111222,8,0,Minima Cuantia,Desierto
 `
 	reader, err := NewProcessCSVReader(strings.NewReader(csvData), 2)
 	if err != nil {
@@ -135,6 +138,9 @@ CO1.P3,900111222,8,0,Minima Cuantia,Desierto
 	}
 	if c1.Records[1].ProveedoresUnicosConRespuestas != 1 {
 		t.Errorf("datos inesperados en registro 1: %+v", c1.Records[1])
+	}
+	if c1.Records[0].NombreEntidad != "ENTIDAD UNO" {
+		t.Errorf("nombre_entidad inesperado en registro 0: %s", c1.Records[0].NombreEntidad)
 	}
 
 	// Chunk 2: debe tener 1 registro
@@ -157,9 +163,9 @@ CO1.P3,900111222,8,0,Minima Cuantia,Desierto
 }
 
 func TestContractCSVReader(t *testing.T) {
-	csvData := `proceso_de_compra,nit_entidad,documento_proveedor,proveedor_adjudicado,valor_del_contrato,codigo_de_categoria_principal,fecha_de_firma
-CO1.P1,900123456,800999000,PROV ACME,150000000,80101500,2026-01-15
-CO1.P2,900654321,800888111,PROV GLOBEX,50000000,72101500,2026-02-20
+	csvData := `nombre_entidad,proceso_de_compra,nit_entidad,documento_proveedor,proveedor_adjudicado,valor_del_contrato,codigo_de_categoria_principal,fecha_de_firma
+ENTIDAD UNO,CO1.P1,900123456,800999000,PROV ACME,150000000,80101500,2026-01-15
+ENTIDAD DOS,CO1.P2,900654321,800888111,PROV GLOBEX,50000000,72101500,2026-02-20
 `
 	reader, err := NewContractCSVReader(strings.NewReader(csvData), 1)
 	if err != nil {
@@ -178,6 +184,9 @@ CO1.P2,900654321,800888111,PROV GLOBEX,50000000,72101500,2026-02-20
 	}
 	if c1.Records[0].ProveedorAdjudicado != "PROV ACME" {
 		t.Errorf("proveedor erróneo: %s", c1.Records[0].ProveedorAdjudicado)
+	}
+	if c1.Records[0].NombreEntidad != "ENTIDAD UNO" {
+		t.Errorf("nombre_entidad erróneo: %s", c1.Records[0].NombreEntidad)
 	}
 
 	c2, err := reader.NextChunk()
